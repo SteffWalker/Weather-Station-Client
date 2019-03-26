8e73e7aed41f8a5bfc24accedda829e596e0e5f2
Steffan Walker <Steffan@pc-131-214.customer.avalon.fortis.lan>
2019-03-26 01:37:41 +0000

    Added initial files for Weather Station Client

---
+++ src/core/StationClient.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package core;
+import java.util.Random;
+
+/**
+ *
+ * @author Steffan
+ */
+public class StationClient {
+    private double longitude, latitude, temp, humidity;
+    
+    StationClient()
+    {
+        longitude = (Math.random() * ((180 - -180) + 1)) + -180;
+        latitude = (Math.random() * ((90 - -90) + 1)) + -90;
+    }
+    
+    public void generateData()
+    {
+        Random rand = new Random();
+        this.temp = rand
+    }
+}
